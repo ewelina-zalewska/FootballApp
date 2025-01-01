ffd21ab46c8527f1ecef23e0a0b602e9902da346
Ewelina <ewe.zale@gmail.com>
2025-01-01 13:38:39 +0100

    fix: change date format

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -2,6 +2,6 @@
 	const year1900 = "1900-01-01";
 	const date = new Date();
 	const yearBefore = `${date.getFullYear() - 1}-${date.getMonth() + 1}-${date.getDate()}`;
-	const now = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+	const now = `${date.getFullYear()}-${("0" + date.getMonth() + 1).slice(-2)}-${("0" + date.getDate()).slice(-2)}`;
 	return { now, year1900, yearBefore };
 };
